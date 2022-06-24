930b6eff5712b1060b9457e7c60930dc61ece77c
Jonnathan <91436348+Jonnathan1288@users.noreply.github.com>
2022-06-24 13:16:54 -0500

    dfasdf
    sdfasdfa

--- JavaApplication21/src/javaapplication21/JavaApplication21.java
+++ JavaApplication21/src/javaapplication21/JavaApplication21.java
@@ -16,10 +16,10 @@ public class JavaApplication21 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int num1 = 100;
+        int num1 = 1000;
         for (int i = 1; i <= num1; i++) {
             
-            System.out.println(""+i);
+            System.out.println("El numero"+i);
         }
     }
     
